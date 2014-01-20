6209f68a655e1cf7e84240a6aa9c624c313d5a9c
bechem <bechem@bechem-desktop.(none)>
2014-01-20 23:03:22 +0100

    Section in admin configuration.

--- app/code/local/Magentos/Tutorial/Block/Hello.php
+++ app/code/local/Magentos/Tutorial/Block/Hello.php
@@ -3,6 +3,7 @@
 class Magentos_Tutorial_Block_Hello extends Mage_Core_Block_Template
 {
 	public function isLoggedIn(){
+		return Mage::getStoreConfig('magentos_sekcja/magentos_grupa/taknie');    
 		return Mage::getSingleton('customer/session')->isLoggedIn();
 	}
 }
---
+++ app/code/local/Magentos/Tutorial/Helper/Data.php
@@ -0,0 +1,2 @@
+<?php 
+class Magentos_Tutorial_Helper_Data extends Mage_Core_Helper_Abstract{}
